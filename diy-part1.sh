06a16d87697ac934ce3c3460e336f9ea6b1cda72
T-Rex <38811303+nasheep@users.noreply.github.com>
2022-07-15 00:03:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-git clone https://github.com/kenzok8/openwrt-packages.git package/lean
+git clone https://github.com/kenzok8/openwrt-packages.git package/lean1
+mv package/lean1/* package/lean
